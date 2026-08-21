c13ebaa9817e2d17659848995c75cd8ef21a46f4
Singam Jashwanth <singamjashwanthyadav@gmail.com>
2026-08-21 14:26:20 +0530

    Create spi_slave_select_lint.tcl

---
+++ scripts/lint/spi_slave_select_lint.tcl
@@ -0,0 +1,18 @@
+set search_path "./"
+set link_library ""
+# Enable Lint
+set_app_var enable_lint true
+set_app_var enable_lint_save true
+# Lint Setup
+configure_lint_setup -goal lint_rtl
+# Top Module
+set top "spi_slave_select_lint.tcl"
+# Analyze RTL
+analyze -format verilog "./rtl/${top}.v"
+# Elaborate
+elaborate $top -verbose
+# Run Lint
+check_lint
+# Report
+#report_violations -app {setup design lint}
+report_lint -verbose -file "${top}_lint_report.txt"
